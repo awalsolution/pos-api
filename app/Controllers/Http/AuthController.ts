151825e227df4b5b5b18d1f7cdc3bcaad5b3bf45
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-22 00:46:36 +0500

    update controllers

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,17 +1,7 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class AuthController {
-  public async index({}: HttpContextContract) {}
+  public async register({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
-
-  public async store({}: HttpContextContract) {}
-
-  public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
+  public async login({}: HttpContextContract) {}
 }
--- start/routes/auth.routes.ts
+++ start/routes/auth.routes.ts
@@ -2,5 +2,5 @@ import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
   Route.post("/auth/login", "AuthController.login");
-  Route.post("/auth/register", "AuthController.login");
+  Route.post("/auth/register", "AuthController.register");
 }).prefix("/v1");
