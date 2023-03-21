e1d7a1c21595580ab73f2de5e31b5b67587c76ab
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-22 01:40:24 +0500

    add user_profile migration

--- database/migrations/1679424946844_users.ts
+++ database/migrations/1679424946844_users.ts
@@ -6,9 +6,24 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id").primary();
-      table.string("email").notNullable().unique();
+      table
+        .integer("role_id")
+        .unsigned()
+        .references("roles.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("email").notNullable().unique().index();
       table.string("password").notNullable();
-      table.string("remember_me_token").nullable().defaultTo(0);
+      table.boolean("is_account_activated").notNullable().defaultTo(0);
+      table.boolean("remember_token").nullable();
+      table.boolean("banned").notNullable().defaultTo(0);
+      table.string("activation_code").nullable().index().unique();
+      table.boolean("login_status").defaultTo(0);
+      table.integer("forgot_password_code").nullable().unique();
+      table.boolean("is_email_verified").notNullable().defaultTo(0);
+      table.timestamp("last_login_time").nullable();
+      table.timestamp("account_activated_at").nullable();
+      table.timestamp("email_verified_at").nullable();
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
