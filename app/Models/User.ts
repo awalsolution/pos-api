0a7cd69c8f0793899a2398672aa131f85d395ce7
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-22 02:32:09 +0500

    modify user model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,30 +1,60 @@
-import { DateTime } from 'luxon'
-import Hash from '@ioc:Adonis/Core/Hash'
-import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import Hash from "@ioc:Adonis/Core/Hash";
+import { column, beforeSave, BaseModel } from "@ioc:Adonis/Lucid/Orm";
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
-  public email: string
+  public roleId: number;
+
+  @column()
+  public email: string;
 
   @column({ serializeAs: null })
-  public password: string
+  public password: string;
 
   @column()
-  public rememberMeToken: string | null
+  public is_account_activated: boolean;
+
+  @column()
+  public banned: boolean;
+
+  @column()
+  public login_status: boolean;
+
+  @column()
+  public is_email_verified: boolean;
+
+  @column()
+  public forgot_password_code: number;
+
+  @column()
+  public rememberMeToken: string | null;
+
+  @column()
+  public activation_code: string | null;
+
+  @column()
+  public last_login_time: DateTime | null;
+
+  @column()
+  public account_activated_at: DateTime | null;
+
+  @column()
+  public email_verified_at: DateTime | null;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 
   @beforeSave()
-  public static async hashPassword (user: User) {
+  public static async hashPassword(user: User) {
     if (user.$dirty.password) {
-      user.password = await Hash.make(user.password)
+      user.password = await Hash.make(user.password);
     }
   }
 }
