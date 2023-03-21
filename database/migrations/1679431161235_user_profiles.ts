e91dddbd56e227cc0320c788f06c70596a8a300e
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-22 01:46:07 +0500

    update profile migration

--- database/migrations/1679431161235_user_profiles.ts
+++ database/migrations/1679431161235_user_profiles.ts
@@ -6,7 +6,23 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-
+      table
+        .integer("user_id")
+        .unsigned()
+        .unique()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      table.string("first_name").nullable();
+      table.string("last_name").nullable();
+      table.string("phone").nullable();
+      table.string("address").nullable();
+      table.string("city").nullable();
+      table.string("zip").nullable();
+      table.string("state").nullable();
+      table.string("country").nullable();
+      table.string("profile_picture").nullable();
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
