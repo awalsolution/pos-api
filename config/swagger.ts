ce5e2a630d85012ca989635d423a6600003e9c9e
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-22 00:55:59 +0500

    add role doc & controller

--- config/swagger.ts
+++ config/swagger.ts
@@ -2,7 +2,7 @@ import { SwaggerConfig } from "@ioc:Adonis/Addons/Swagger";
 
 export default {
   uiEnabled: true, //disable or enable swaggerUi route
-  uiUrl: "docs", // url path to swaggerUI
+  uiUrl: "api", // url path to swaggerUI
   specEnabled: true, //disable or enable swagger.json route
   specUrl: "/swagger.json",
 
@@ -18,7 +18,7 @@ export default {
       },
     },
 
-    apis: ["app/**/*.ts", "docs/swagger/**/*.yml", "start/index.ts"],
+    apis: ["app/**/*.ts", "docs/**/*.yml", "start/index.ts"],
     basePath: "/",
   },
   mode: process.env.NODE_ENV === "production" ? "PRODUCTION" : "RUNTIME",
