bef6cf615e6890321aa09718b915517bc2d66fdc
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-22 20:39:56 +0500

    add seeder

--- database/seeders/Role.ts
+++ database/seeders/Role.ts
@@ -1,7 +1,17 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import Role from "App/Models/Role";
 
 export default class extends BaseSeeder {
-  public async run () {
-    // Write your database queries inside the run method
+  public async run() {
+    await Role.createMany([
+      {
+        name: "onwer",
+        description: "Application Onwer has full permissions",
+      },
+      {
+        name: "user",
+        description: "this one is simple customer",
+      },
+    ]);
   }
 }
--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,7 +1,30 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import { DateTime } from "luxon";
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import User from "App/Models/User";
+import UserProfile from "App/Models/UserProfile";
 
 export default class extends BaseSeeder {
-  public async run () {
-    // Write your database queries inside the run method
+  public async run() {
+    await User.createMany([
+      {
+        email: "onwer@gmail.com",
+        password: "123456",
+        is_account_activated: true,
+        is_email_verified: true,
+        email_verified_at: DateTime.now(),
+      },
+    ]);
+    await UserProfile.createMany([
+      {
+        userId: 1,
+        first_name: "onwer",
+        last_name: "app",
+        phone_number: "123456789",
+        address: "Lahore",
+        city: "Lahore",
+        state: "Punjab",
+        country: "Pakistan",
+      },
+    ]);
   }
 }
