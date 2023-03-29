9f6be5caee59e6049674e047152f40018c6e5cdd
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2023-03-29 06:19:57 -0700

    update code

--- start/routes/api/acl/roles.ts
+++ start/routes/api/acl/roles.ts
@@ -6,22 +6,22 @@ Route.group(async () => {
   Route.post("/", (ctx: HttpContextContract) => {
     return new RolesController().create(ctx);
   });
-  // Route.put('/:id',(ctx: HttpContextContract) => {
-  //   return new RolesController().update(ctx)
-  // })
-  // Route.delete('/:id',(ctx: HttpContextContract) => {
-  //   return new RolesController().destroy(ctx)
-  // })
+  Route.put("/:id", (ctx: HttpContextContract) => {
+    return new RolesController().update(ctx);
+  });
+  Route.delete("/:id", (ctx: HttpContextContract) => {
+    return new RolesController().destroy(ctx);
+  });
 
-  // Route.get('/',(ctx: HttpContextContract) => {
-  //   return new RolesController().find(ctx)
-  // })
-  // Route.get('/:id',(ctx: HttpContextContract) => {
-  //   return new RolesController().get(ctx)
-  // })
-  // Route.group(async () => {
-  //   Route.put('/:id',(ctx: HttpContextContract) => {
-  //     return new RolesController().get(ctx)
-  //   })
-  // }).prefix('users')
-}).prefix("/api/acl/roles");
+  Route.get("/", (ctx: HttpContextContract) => {
+    return new RolesController().find(ctx);
+  });
+  Route.get("/:id", (ctx: HttpContextContract) => {
+    return new RolesController().get(ctx);
+  });
+  Route.group(async () => {
+    Route.put("/:id", (ctx: HttpContextContract) => {
+      return new RolesController().get(ctx);
+    });
+  }).prefix("users");
+}).prefix("/v1/roles");
