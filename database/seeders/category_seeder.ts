f39d5d0b638077fae8a7637b92ce3e3fea5beda1
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2024-03-11 11:45:38 +0500

    update category image

--- database/seeders/category_seeder.ts
+++ database/seeders/category_seeder.ts
@@ -7,35 +7,35 @@ export default class extends BaseSeeder {
       {
         shopId: 1,
         name: 'cloths',
-        thumbnail: '/uploads/products/h3uff8j6uzx2ebw7gn6lyqfd.jpg',
+        thumbnail: '/uploads/products/r3os2cmjybc22ncnimqftpdb.jpg',
       },
       {
         shopId: 2,
         name: 'shoes',
-        thumbnail: '/uploads/products/wmc37mdsz1j0rvnanvbugs1p.jpg',
+        thumbnail: '/uploads/products/r3os2cmjybc22ncnimqftpdb.jpg',
       },
       {
         shopId: 3,
         name: 'laptops',
-        thumbnail: '/uploads/products/c4tal7w5erf6l8zuwnlb0xj4.jp',
+        thumbnail: '/uploads/products/r3os2cmjybc22ncnimqftpdb.jpg',
       },
       {
         shopId: 1,
         name: 'cloths child',
         parent_id: 1,
-        thumbnail: '/uploads/products/c4tal7w5erf6l8zuwnlb0xj4.jp',
+        thumbnail: '/uploads/products/r3os2cmjybc22ncnimqftpdb.jpg',
       },
       {
         shopId: 1,
         name: 'shoes child',
         parent_id: 2,
-        thumbnail: '/uploads/products/c4tal7w5erf6l8zuwnlb0xj4.jp',
+        thumbnail: '/uploads/products/r3os2cmjybc22ncnimqftpdb.jpg',
       },
       {
         shopId: 2,
         name: 'laptops child',
         parent_id: 3,
-        thumbnail: '/uploads/products/c4tal7w5erf6l8zuwnlb0xj4.jp',
+        thumbnail: '/uploads/products/r3os2cmjybc22ncnimqftpdb.jpg',
       },
     ])
   }
