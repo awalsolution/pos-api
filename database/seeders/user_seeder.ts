127ccf982281c011cd12099f420ed587e713d8ab
IQBAL HASSAN <iqbal.hassan2584@gmail.com>
2024-10-02 10:35:33 +0500

    fix tenant user issue

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,31 +1,24 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import User from '#models/user'
+import { DateTime } from 'luxon'
 
 export default class extends BaseSeeder {
   async run() {
     await User.createMany([
       {
-        email: 'admin@gmail.com',
-        password: 'admin@123',
-        created_by: 'Iqbal Hassan',
         name: 'Iqbal Hassan',
+        email: 'admin@gmail.com',
         phone_number: '12345678',
-        address: 'DHA Phase 5',
-        city: 'Lahore',
-        state: 'Punjab',
-        country: 'Pakistan',
-        profile_picture: '/uploads/admin/dev.jpg',
-      },
-      {
-        email: 'manager@gmail.com',
         password: 'admin@123',
-        created_by: 'Iqbal Hassan',
-        name: 'Jawad Ali',
-        phone_number: '12345678',
+        is_email_verified: 1,
+        email_verified_at: DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss'),
+        is_phone_verified: 1,
+        phone_verified_at: DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss'),
         address: 'DHA Phase 5',
         city: 'Lahore',
         state: 'Punjab',
         country: 'Pakistan',
+        created_by: 'Iqbal Hassan',
         profile_picture: '/uploads/admin/dev.jpg',
       },
     ])
